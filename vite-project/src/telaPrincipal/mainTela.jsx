b66688e02a06ba747e0958a240bf28acb49a5281
Talles Pellense <160547176+tallesSENAI@users.noreply.github.com>
2024-06-04 21:39:29 -0300

    Add files via upload

---
+++ vite-project/src/telaPrincipal/mainTela.jsx
@@ -0,0 +1,4 @@
+//TELA PRINCIPAL
+
+/*aqui serão apresentados na tela: os anúncios disponíveis,
+botão ANUNCIAR, botão PESQUISAR, botão FILTRAR e entre outros que vamos ir decidindo*/
---
+++ vite-project/src/telaUsuario/cadastroUsuário.jsx
@@ -0,0 +1,7 @@
+//CADASTRO DO USUARIO
+
+//Para fazer um novo cadastro, o usuário deve preencher os seguintes campos:
+//- Nome e sobrenome
+//- E-mail
+//- Número de telefone
+//- Senha até 30 dígitos
