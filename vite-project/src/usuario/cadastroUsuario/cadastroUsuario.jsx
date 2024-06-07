eb4cfdf416a2ecd47d52f39e3fccce64b50547d1
Erik Motta <erikmotta1@hotmail.com>
2024-06-07 20:16:51 -0300

    adicionando elementos na tela cadastro (não esta funcionando ainda)

--- vite-project/src/usuario/cadastroUsuario/cadastroUsuario.jsx
+++ vite-project/src/usuario/cadastroUsuario/cadastroUsuario.jsx
@@ -5,11 +5,20 @@
 //- E-mail
 //- Número de telefone
 //- Senha até 30 dígitos
-
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Cadastrar(){
+    
+    const [nome, setNome] = useState('');
+    const [senha, setSenha] = useState('');
 
+    setListaCadastro({
+        nome: nome,
+        email: email,
+        telefone: telefone,
+        senha: senha
+    })
 }
 
 function CadastroUsuario(){
@@ -17,8 +26,18 @@ function CadastroUsuario(){
         <>
         <Link to={'/'}>Início</Link>
         <h1>Cadastro de Usuário</h1>
+
         <label htmlFor="nome">Nome: </label>
-        <input type="text" placeholder="Digite seu nome" />
+        <input type="text" placeholder="Digite seu nome completo" />
+        <br></br>
+        <label htmlFor="email">Email: </label>
+        <input type="text" placeholder="Digite seu email" />
+        <br></br>
+        <label htmlFor="telefone">Telefone: </label>
+        <input type="number" placeholder="Digite seu telefone" />
+        <br></br>
+        <label htmlFor="senha">Senha: </label>
+        <input type="password" placeholder="Sua senha até 9 dígitos" />
         <br></br>
         <br></br>
         <button onClick={Cadastrar}>Cadastrar</button>
