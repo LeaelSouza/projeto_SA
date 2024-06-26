8dd193b0faec50b9888caa2e2b4ff182db726016
Erik Motta <erikmotta1@hotmail.com>
2024-06-26 19:43:20 -0300

    deixando obrigatorio preencher os campos

--- vite-project/src/usuario/loginUsuario/loginUsuario.jsx
+++ vite-project/src/usuario/loginUsuario/loginUsuario.jsx
@@ -51,10 +51,10 @@ function LoginUsuario() {
             <form onSubmit={handleSubmit}>
 
                 <label htmlFor="email"><b>Email: </b></label>
-                <input type="email" id="email" name="email" placeholder="Digite seu email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                <input required type="email" id="email" name="email" placeholder="Digite seu email" value={email} onChange={(e) => setEmail(e.target.value)}/>
                 <br></br>
                 <label htmlFor="senha"><b>Senha: </b></label>
-                <input type="senha" id="senha" name="senha" placeholder="Digite sua senha" value={senha} onChange={(e) => setSenha(e.target.value)} />
+                <input required type="senha" id="senha" name="senha" placeholder="Digite sua senha" value={senha} onChange={(e) => setSenha(e.target.value)} />
                 <br></br><br></br>
                 <button type="submit">Entrar</button>
             </form>
