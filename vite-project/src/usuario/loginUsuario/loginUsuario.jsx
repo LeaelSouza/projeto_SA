6471c6fc44b7130dfa9ea92cbe205dedc11e0074
Leael Thiago de Souza <160546949+LeaelSouza@users.noreply.github.com>
2024-06-25 23:11:22 -0300

    Login usuário

--- vite-project/src/usuario/loginUsuario/loginUsuario.jsx
+++ vite-project/src/usuario/loginUsuario/loginUsuario.jsx
@@ -5,30 +5,74 @@
 //- Senha
 
 import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 
 function LoginUsuario() {
+
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+
+    //alterado por Talles
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const usuario = {
+            emailCadastroUsuario: email,
+            senhaCadastroUsuario: senha,
+        }
+
+        //post USUARIO
+        fetch('http://localhost:4300/usuario/login', {
+        method: 'POST',
+        headers:{
+            "Content-type": 'application/json',
+        },
+        body: JSON.stringify(usuario)
+        })
+        .then(a => a.json())
+        .then(resultado => {
+
+            console.log(resultado)
+
+            if (resultado.autenticado === true){
+                alert("Login efetuado com sucesso!")
+            }
+                else {
+                    alert(resultado.message)
+                }
+        })
+    };
+
+
     return (
         <div>
             <h1>Login</h1>
 
-            <label htmlFor="email"><b>Email: </b></label>
-            <input
+            <form onSubmit={handleSubmit}>
+
+                <label htmlFor="email"><b>Email: </b></label>
+                <input
                 type="email"
                 id="email"
                 name="email"
                 placeholder="Digite seu email"
-            />
-            <br></br>
-            <label htmlFor="senha"><b>Senha: </b></label>
-            <input
+                value={email} onChange={(e) => setEmail(e.target.value)}
+                />
+                <br></br>
+
+                <label htmlFor="senha"><b>Senha: </b></label>
+                <input
                 type="senha"
                 id="senha"
                 name="senha"
                 placeholder="Digite sua senha"
-            />
-            <br></br>
-            <br></br>
-            <button type="submit">Entrar</button>
+                value={senha} onChange={(e) => setSenha(e.target.value)}
+                />
+
+                <br></br>
+                <br></br>
+                <button type="submit">Entrar</button>
+            </form>
         </div>
     )
 }
